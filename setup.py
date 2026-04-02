d692aee337cc8cd0047a6073f5c4b7bde8c29e86
Ristoll <chamata.kristina@gmail.com>
2026-04-02 16:43:30 +0300

    Add CI workflow checks for pull requests and include pytest in requirements

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 # test commit to trigger GitHub Actions
 # one more test commit to trigger GitHub Actions
 # one more test commit to trigger GitHub Actions
+
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
